2d618fde0209de99e4a224a79a828dc41980d033
cerenlale <ceren.lale@sabanciuniv.edu>
2026-01-05 21:14:07 +0300

    Implement widget test for course card image display
    Added a widget test to verify the display of the course card image.

--- finalcodes/suboxd/test/widget_test.dart
+++ finalcodes/suboxd/test/widget_test.dart
@@ -1,30 +1,51 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:suboxd/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const SUboxdApp());
+  testWidgets('The course card image should be displayed correctly.', (WidgetTester tester) async {
+    const String testImagePath = 'assets/hum201.png';
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: TestCourseCard(
+            width: 100,
+            height: 150,
+            imagePath: testImagePath,
+          ),
+        ),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final imageFinder = find.byType(Image);
+    expect(imageFinder, findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final Image imageWidget = tester.widget(imageFinder);
+    expect((imageWidget.image as AssetImage).assetName, testImagePath);
   });
+}
+
+class TestCourseCard extends StatelessWidget {
+  final double width, height;
+  final String imagePath;
+
+  const TestCourseCard({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(14),
+      child: Image.asset(
+        imagePath,
+        width: width,
+        height: height,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
 }
